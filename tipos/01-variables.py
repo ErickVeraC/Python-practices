c847cd2c9ce2fa407cfc7d4548d62ac7f93b9197
Erick Vera <erickverac90@gmail.com>
2024-09-06 17:08:01 -0600

    Agregando documentos de variables

---
+++ tipos/01-variables.py
@@ -0,0 +1,10 @@
+nombre_curso = "Python"
+nombre1 = "Todo"
+NOMBRE_CURSO = "es"
+# No es recomendable usar combiaciones de mayusculas y minusculas
+NoMbRe_CuRsP = "increible"
+NombreCurso = "Saludos"
+print(nombre_curso, nombre1, NOMBRE_CURSO, NoMbRe_CuRsP, NombreCurso)
+alumnos = 5000  # Integer o Entero
+puntaje = 9.9  # Float o Decimal
+publicado = True  # Booleano para que funcione la T es mayusucula
